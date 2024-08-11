053b112abeff8e0c5c3ed754ac2b1b713d08e376
riannerii <160463647+riannerii@users.noreply.github.com>
2024-08-11 23:51:58 +0800

    Add files via upload

---
+++ view-account/view-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-account.component.html',
+  styleUrl: './view-account.component.css'
+})
+export class ViewAccountComponent {
+
+}
