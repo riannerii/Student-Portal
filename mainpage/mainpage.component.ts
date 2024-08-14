85bdcfca1642fa689eb86d5f46f263e432ce2692
riannerii <160463647+riannerii@users.noreply.github.com>
2024-08-14 11:01:00 +0800

    *updated mainpage (toolbar)

--- mainpage/mainpage.component.ts
+++ mainpage/mainpage.component.ts
@@ -1,15 +1,18 @@
 import { Component, computed, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatBadgeModule } from '@angular/material/badge'
+import { MatMenuModule } from '@angular/material/menu'
 import { SidenavComponent } from "../sidenav/sidenav.component";
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, SidenavComponent],
+  imports: [RouterLink, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, SidenavComponent, MatBadgeModule, MatMenuModule, MatListModule],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
